1ee4b26c305d49047179acc76a3d50370bada0ca
Felipe Oucharski <felipe.oucharski@andritz.com>
2024-07-11 19:48:44 +0200

    fix: add prepending slash do default logo

--- settings/layout.ts
+++ settings/layout.ts
@@ -1,55 +1,55 @@
-import React from 'react';
+import React from "react";
 
-import { LayoutProviderProps } from '@andritz/hwf2';
-import Router from 'next/router';
+import { LayoutProviderProps } from "@andritz/hwf2";
+import Router from "next/router";
 
-const HomeI = () => React.createElement('span', null, '🏠');
-const ProjectI = () => React.createElement('span', null, '🚀');
-const TestI = () => React.createElement('span', null, '👽');
-const TestII = () => React.createElement('span', null, '👨‍🚀');
-const SettingsI = () => React.createElement('span', null, '⚙️');
+const HomeI = () => React.createElement("span", null, "🏠");
+const ProjectI = () => React.createElement("span", null, "🚀");
+const TestI = () => React.createElement("span", null, "👽");
+const TestII = () => React.createElement("span", null, "👨‍🚀");
+const SettingsI = () => React.createElement("span", null, "⚙️");
 
 export const layout: LayoutProviderProps = {
   menuItemsData: [
-    { 
-      text: "Hello World", 
+    {
+      text: "Hello World",
       icon: HomeI,
-      onClick: () => Router.push('/'),
+      onClick: () => Router.push("/"),
       context: ["sidebar", "topbar", "accountMenu"],
     },
-    { 
-      text: "Another Route", 
-      icon: ProjectI, 
-      onClick: () => Router.push('/another-route'),
+    {
+      text: "Another Route",
+      icon: ProjectI,
+      onClick: () => Router.push("/another-route"),
       context: ["sidebar", "topbar"],
     },
-    { 
-      text: "Drawer Route", 
-      icon: TestI, 
-      onClick: () => Router.push('/another-route'),
+    {
+      text: "Drawer Route",
+      icon: TestI,
+      onClick: () => Router.push("/another-route"),
       context: ["sidebar"],
     },
-    { 
-      text: "Topbar Route", 
-      icon: TestII, 
-      onClick: () => Router.push('/another-route'),
-      context: [ "topbar"],
+    {
+      text: "Topbar Route",
+      icon: TestII,
+      onClick: () => Router.push("/another-route"),
+      context: ["topbar"],
     },
-    { 
-      text: "Disabled Route", 
-      icon: TestII, 
-      onClick: () => Router.push('/another-route'),
-      context: [ "topbar", "sidebar", "accountMenu"],
+    {
+      text: "Disabled Route",
+      icon: TestII,
+      onClick: () => Router.push("/another-route"),
+      context: ["topbar", "sidebar", "accountMenu"],
       disabled: true,
     },
-    { 
-      text: "Settings", 
-      icon: SettingsI, 
-      onClick: () => Router.push('/settings'),
+    {
+      text: "Settings",
+      icon: SettingsI,
+      onClick: () => Router.push("/settings"),
       context: ["accountMenu"],
     },
   ],
   baseSideBarWidth: 240,
-  logoSrc: "img/logo.svg",
-  footer: { text: `Andritz Hydro © ${new Date().getFullYear()}`, },
+  logoSrc: "/img/logo.svg",
+  footer: { text: `Andritz Hydro © ${new Date().getFullYear()}` },
 };
