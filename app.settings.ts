f9f6472e3f6500383e4e02a39b0ccd3b0025b88f
Felipe Oucharski <felipe.oucharski@andritz.com>
2024-07-26 15:13:25 +0200

    refactor: Remove unused app settings

--- app.settings.ts
+++ app.settings.ts
@@ -1,7 +1,4 @@
 export const appSettings = {
   title: "Andritz Tool",
   description: "A starter for Next.js with Material-UI and TypeScript",
-  mainColor: "#0075be",
-  secondaryColor: "#666",
-  favicon: "/favicon.ico",
 };
